10ff2e882d152c727583394868caea2da8c6aad5
Dhananjay Sachdeva <dhananjay.sachdeva@gmail.com>
2020-07-14 10:45:44 +0530

    Added functionality to check for tie or win

--- flipCoinSim.sh
+++ flipCoinSim.sh
@@ -3,17 +3,32 @@ function flipCoin() {
 	echo $(( RANDOM%2 ))
 }
 isHeads=1
-tailsWon=0
-headsWon=0
-for (( ctr=1; ctr<=10; ctr++ ))
+tailWin=0
+headWin=0
+isTie=0
+while [ $headWin -le 21 ] && [ $tailWin -le 21 ]
 do
-	coinToss="$( flipCoin )"
-	if [ $coinToss -eq $isHeads ]
+	if [ $headWin -eq 21 ] && [ $tailWin -eq 21 ]
 	then
-		(( headsWon++ ))
-	else
-		(( tailsWon++ ))
+		isTie=1
+		break
 	fi
+        coinToss="$( flipCoin )"
+        if [ $coinToss -eq $isHeads ]
+        then
+                (( headWin++ ))
+        else
+                (( tailWin++ ))
+        fi
 done
-echo "Tails won "$tailsWon" number of times"
-echo "Heads won "$headsWon" number of times"
+if [ $isTie -eq 1 ]
+then
+	echo "Tie game"
+else
+	if [ $headWin -gt $tailWin ]
+	then
+		echo "Head won by "$(( $headWin-$tailWin ))
+	else
+		echo "Tail won by "$(( $tailWin-$headWin ))
+	fi
+fi
