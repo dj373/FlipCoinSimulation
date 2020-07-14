dd9b26d9ea767fe23f44c804842486f0d3107d13
Dhananjay Sachdeva <dhananjay.sachdeva@gmail.com>
2020-07-14 10:33:42 +0530

    Added loop to iterate multiple simulations of coin flip

--- flipCoinSim.sh
+++ flipCoinSim.sh
@@ -3,10 +3,17 @@ function flipCoin() {
 	echo $(( RANDOM%2 ))
 }
 isHeads=1
-coinToss="$( flipCoin )"
-if [ $coinToss -eq $isHeads ]
-then
-	echo "Heads Won"
-else
-	echo "Tails Won"
-fi
+tailsWon=0
+headsWon=0
+for (( ctr=1; ctr<=10; ctr++ ))
+do
+	coinToss="$( flipCoin )"
+	if [ $coinToss -eq $isHeads ]
+	then
+		(( headsWon++ ))
+	else
+		(( tailsWon++ ))
+	fi
+done
+echo "Tails won "$tailsWon" number of times"
+echo "Heads won "$headsWon" number of times"
